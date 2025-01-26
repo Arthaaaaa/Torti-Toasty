957ed147987970c3bb8a7fd02b7da95881495aee
Arthaaaaa <zackyartha@gmail.com>
2025-01-26 21:13:17 +0700

    Fix main route and clean up code

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask , render_template, redirect, url_for, request, session
 
 
 app = Flask(__name__)
+app.run(host="0.0.0.0", port=8080)
 app.secret_key = 'admin'  # Replace with a secure key
 
 
 @app.route("/home")
 def home():
     return render_template("index.html")
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
 
 
 # Add login route
